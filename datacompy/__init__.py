d6db60f642343994790f697a3c8a79ec3b6d7c5c
Faisal <faisal.dosani@protonmail.com>
2023-09-25 13:48:54 -0400

    bump version

--- datacompy/__init__.py
+++ datacompy/__init__.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.10.3"
+__version__ = "0.10.4"
 
 from datacompy.core import *
 from datacompy.fugue import (
