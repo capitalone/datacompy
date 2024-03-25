21a27a95dddc8d8fb70cab21e6894ed0d9c9eb85
Faisal <faisal.dosani@pm.me>
2024-03-25 11:43:29 -0300

    non-join column unicode fix (#285)
    * fixes #284
    
    * bump version

--- tests/test_spark.py
+++ tests/test_spark.py
@@ -2094,15 +2094,22 @@ def text_alignment_validator(
         if not at_column_section and section_start in line:
             at_column_section = True
 
+
 def test_unicode_columns(spark_session):
     df1 = spark_session.createDataFrame(
-        [{"a": 1, "例": 2}, {"a": 1, "例": 3}]
+        [
+            (1, "foo", "test"),
+            (2, "bar", "test"),
+        ],
+        ["id", "例", "予測対象日"],
     )
     df2 = spark_session.createDataFrame(
-        [{"a": 1, "例": 2}, {"a": 1, "例": 3}]
+        [
+            (1, "foo", "test"),
+            (2, "baz", "test"),
+        ],
+        ["id", "例", "予測対象日"],
     )
-    compare = SparkCompare(
-        spark_session, df1, df2, join_columns=["例"]
-    )
+    compare = SparkCompare(spark_session, df1, df2, join_columns=["例"])
     # Just render the report to make sure it renders.
     compare.report()
