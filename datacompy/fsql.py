1605725c884a1f78022a193095fd339384581ae8
Faisal <faisal.dosani@pm.me>
2024-04-22 09:00:42 -0400

    Update datacompy/fsql.py

--- datacompy/fsql.py
+++ datacompy/fsql.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2020 Capital One Services, LLC
+# Copyright 2024 Capital One Services, LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
