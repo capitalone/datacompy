4005609eaab949b483980f0390221837eb4736a2
Faisal Dosani <faisal.dosani@pm.me>
2024-07-10 11:34:39 -0300

    bumping version

--- datacompy/__init__.py
+++ datacompy/__init__.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.13.1"
+__version__ = "0.13.2"
 
 import platform
 from warnings import warn
