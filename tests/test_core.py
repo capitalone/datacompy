9dd08b6e7c2fc3aac10008e472712425577db0fd
Faisal Dosani <faisal.dosani@pm.me>
2025-02-25 19:32:31 -0500

    additonal tests for test_non_full_join_counts

--- tests/test_core.py
+++ tests/test_core.py
@@ -1379,7 +1379,7 @@ def test_full_join_counts_no_matches():
     assert not compare.matches()
     assert compare.all_columns_match()
     assert not compare.all_rows_overlap()
-    assert compare.intersect_rows_match()
+    assert not compare.intersect_rows_match()
     assert compare.count_matching_rows() == 0
     assert_frame_equal(
         compare.sample_mismatch(column="a").sort_index(),
@@ -1410,3 +1410,39 @@ def test_full_join_counts_some_matches():
     assert_frame_equal(
         compare.all_mismatch(), pd.concat([compare.df1_unq_rows, compare.df2_unq_rows])
     )
+
+
+def test_non_full_join_counts_no_matches():
+    df1 = pd.DataFrame([{"a": 1, "b": 2, "c": 4}, {"a": 1, "b": 3, "c": 4}])
+    df2 = pd.DataFrame([{"a": 1, "b": 4, "d": 5}, {"a": 1, "b": 5, "d": 5}])
+    compare = datacompy.Compare(df1, df2, ["a", "b"], ignore_spaces=False)
+    assert not compare.matches()
+    assert not compare.all_columns_match()
+    assert not compare.all_rows_overlap()
+    assert not compare.intersect_rows_match()
+    assert compare.count_matching_rows() == 0
+    assert_frame_equal(
+        compare.sample_mismatch(column="a").sort_index(),
+        pd.concat(
+            [compare.df1_unq_rows[["a"]], compare.df2_unq_rows[["a"]]]
+        ).sort_index(),
+    )
+    assert compare.all_mismatch().empty  # all_mismatch should be empty
+
+
+def test_non_full_join_counts_some_matches():
+    df1 = pd.DataFrame([{"a": 1, "b": 2, "c": 4}, {"a": 1, "b": 3, "c": 4}])
+    df2 = pd.DataFrame([{"a": 1, "b": 2, "d": 5}, {"a": 1, "b": 5, "d": 5}])
+    compare = datacompy.Compare(df1, df2, ["a", "b"], ignore_spaces=False)
+    assert not compare.matches()
+    assert not compare.all_columns_match()
+    assert not compare.all_rows_overlap()
+    assert compare.intersect_rows_match()
+    assert compare.count_matching_rows() == 1
+    assert_frame_equal(
+        compare.sample_mismatch(column="a").sort_index(),
+        pd.concat(
+            [compare.df1_unq_rows[["a"]], compare.df2_unq_rows[["a"]]]
+        ).sort_index(),
+    )
+    assert compare.all_mismatch().empty  # all_mismatch should be empty
