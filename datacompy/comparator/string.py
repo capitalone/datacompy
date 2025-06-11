143d4363676277431f1fce69a5c312f8cd1bafb5
Faisal Dosani <faisal.dosani@pm.me>
2025-06-11 18:30:17 -0400

    adding string comparator

--- datacompy/comparator/string.py
+++ datacompy/comparator/string.py
@@ -16,13 +16,11 @@
 """String Comparator Class."""
 
 import logging
-from functools import singledispatch
-from typing import Any
 
 import pandas as pd
 import polars as pl
 
-from datacompy.comparator.base import BaseComparator
+from datacompy.comparator.base import BaseStringComparator
 from datacompy.polars import STRING_TYPE as POLARS_STRING_TYPE
 
 DEFAULT_VALUE = "DATACOMPY_NULL"
@@ -47,9 +45,8 @@ except ImportError:
     )
 
 
-# concrete implementation of the BaseComparator for string columns
-class StringComparator(BaseComparator):
-    """Comparator for string columns.
+class PolarsStringComparator(BaseStringComparator):
+    """Comparator for string columns in Polars.
 
     Parameters
     ----------
@@ -59,81 +56,227 @@ class StringComparator(BaseComparator):
         Whether to ignore case when comparing strings.
     """
 
-    def __init__(self, ignore_space: bool = True, ignore_case: bool = True):
-        self.ignore_space = ignore_space
-        self.ignore_case = ignore_case
-
-    # abstract method which uses singledispatching
-    @singledispatch
-    def compare(self, col1: Any, col2: Any) -> Any:
-        """Compare two columns to determine if they are equal.
+    def compare(self, col1: pl.Series, col2: pl.Series) -> pl.Series:
+        """Compare two Polars Series column-wise, taking into account optional normalization for spaces and case sensitivity.
 
         Parameters
         ----------
-        col1 : Any
-            The first column to compare.
-        col2 : Any
-            The second column to compare.
+        col1 : pl.Series
+            The first Polars Series to compare.
+        col2 : pl.Series
+            The second Polars Series to compare.
 
         Returns
         -------
-        Any
+        pl.Series
+            A Polars Series of boolean values where each element indicates
+            whether the corresponding elements in `col1` and `col2` are equal.
+            Handles missing values by treating nulls as equal.
 
         Raises
         ------
-        NotImplementedError
-            If the column types are unsupported.
+        Exception
+            If the comparison fails due to incompatible types or other issues,
+            attempts to cast both columns to strings for comparison. If this
+            also fails, returns a Series of `False` values with the same length
+            as `col1`.
         """
-        raise NotImplementedError("Unsupported column types")
+        if (col1.shape == col2.shape) and (
+            str(col1.dtype.base_type()) in POLARS_STRING_TYPE
+            and str(col2.dtype.base_type()) in POLARS_STRING_TYPE
+        ):
+            col1 = polars_normalize_string_column(
+                col1, self.ignore_space, self.ignore_case
+            )
+            col2 = polars_normalize_string_column(
+                col2, self.ignore_space, self.ignore_case
+            )
+            try:
+                return pl.Series(
+                    (col1.eq_missing(col2)) | (col1.is_null() & col2.is_null())
+                )
+            except Exception:
+                try:
+                    return pl.Series(col1.cast(pl.String) == col2.cast(pl.String))
+                except Exception:
+                    return pl.Series([False] * col1.shape[0])
+        else:
+            return pl.Series([False] * col1.shape[0])
 
-    # pandas implementation of compare
-    @compare.register
-    def _(self, col1: pd.Series, col2: pd.Series) -> pd.Series:
-        col1 = pandas_normalize_string_column(col1, self.ignore_space, self.ignore_case)
-        col2 = pandas_normalize_string_column(col2, self.ignore_space, self.ignore_case)
+
+class PandasStringComparator(BaseStringComparator):
+    """Comparator for string columns in Pandas.
+
+    Parameters
+    ----------
+    ignore_space : bool
+        Whether to ignore leading and trailing whitespace when comparing strings.
+    ignore_case : bool
+        Whether to ignore case when comparing strings.
+    """
+
+    def compare(self, col1: pd.Series, col2: pd.Series) -> pd.Series:
+        """Compare two Pandas Series column-wise, taking into account optional normalization for spaces and case sensitivity.
+
+        Parameters
+        ----------
+        col1 : pd.Series
+            The first Pandas Series to compare.
+        col2 : pd.Series
+            The second Pandas Series to compare.
+
+        Returns
+        -------
+        pd.Series
+            A Pandas Series of boolean values where each element indicates
+            whether the corresponding elements in `col1` and `col2` are equal.
+            Handles missing values by treating nulls as equal.
+
+        Raises
+        ------
+        Exception
+            If the comparison fails due to incompatible types or other issues,
+            attempts to cast both columns to strings for comparison. If this
+            also fails, returns a Series of `False` values with the same length
+            as `col1`.
+        """
+        if (col1.shape == col2.shape) and (
+            (pd.api.types.is_string_dtype(col1) and pd.api.types.is_string_dtype(col2))
+            or (  # infer_dtype is also a string
+                pd.api.types.infer_dtype(col1) == "string"
+                and pd.api.types.infer_dtype(col2) == "string"
+            )
+        ):
+            col1 = pandas_normalize_string_column(
+                col1, self.ignore_space, self.ignore_case
+            )
+            col2 = pandas_normalize_string_column(
+                col2, self.ignore_space, self.ignore_case
+            )
+            try:
+                return pd.Series(
+                    (col1.fillna(DEFAULT_VALUE) == col2.fillna(DEFAULT_VALUE))
+                    | (col1.isnull() & col2.isnull())
+                )
+            except Exception:
+                try:
+                    return pd.Series(col1.astype(str) == col2.astype(str))
+                except Exception:
+                    return pd.Series(False * col1.shape[0])
+        else:
+            return pd.Series([False] * col1.shape[0])
+
+
+class SparkStringComparator(BaseStringComparator):
+    """Comparator for string columns in PySpark.
+
+    Parameters
+    ----------
+    ignore_space : bool
+        Whether to ignore leading and trailing whitespace when comparing strings.
+    ignore_case : bool
+        Whether to ignore case when comparing strings.
+    """
+
+    def compare(
+        self, dataframe: ps.DataFrame, col1: str, col2: str, col_match: str
+    ) -> ps.DataFrame:
+        """Compare two columns in a PySpark DataFrame for string equality.
+
+        Parameters
+        ----------
+        dataframe : pyspark.sql.DataFrame
+            The PySpark DataFrame containing the columns to compare.
+        col1 : str
+            The name of the first column to compare.
+        col2 : str
+            The name of the second column to compare.
+        col_match : str
+            The name of the output column that will store the comparison results.
+
+        Returns
+        -------
+        pyspark.sql.DataFrame
+            The DataFrame with an additional column containing the comparison results.
+
+        Raises
+        ------
+        Exception
+            If the comparison fails due to incompatible types or other issues,
+            returns a column of `False` values.
+        """
         try:
-            return pd.Series(
-                (col1.fillna(DEFAULT_VALUE) == col2.fillna(DEFAULT_VALUE))
-                | (col1.isnull() & col2.isnull())
+            col1 = spark_normalize_string_column(
+                psf.col(col1), self.ignore_space, self.ignore_case
+            )
+            col2 = spark_normalize_string_column(
+                psf.col(col2), self.ignore_space, self.ignore_case
+            )
+
+            return dataframe.withColumn(
+                col_match,
+                psf.when(col1.eqNullSafe(col2), psf.lit(True)).otherwise(
+                    psf.lit(False)
+                ),
             )
         except Exception:
-            try:
-                return pd.Series(col1.astype(str) == col2.astype(str))
-            except Exception:
-                return pd.Series(False * col1.shape[0])
+            return dataframe.withColumn(col_match, psf.lit(False))
 
-    # polars implementation of compare
-    @compare.register
-    def _(self, col1: pl.Series, col2: pl.Series) -> pl.Series:
-        col1 = polars_normalize_string_column(col1, self.ignore_space, self.ignore_case)
-        col2 = polars_normalize_string_column(col2, self.ignore_space, self.ignore_case)
+
+class SnowflakeStringComparator(BaseStringComparator):
+    """Comparator for string columns in Snowflake.
+
+    Parameters
+    ----------
+    ignore_space : bool
+        Whether to ignore leading and trailing whitespace when comparing strings.
+    ignore_case : bool
+        Whether to ignore case when comparing strings.
+    """
+
+    def compare(
+        self, dataframe: sp.DataFrame, col1: str, col2: str, col_match: str
+    ) -> sp.DataFrame:
+        """Compare two columns in a Snowflake DataFrame for string equality.
+
+        Parameters
+        ----------
+        dataframe : snowflake.snowpark.DataFrame
+            The Snowflake DataFrame containing the columns to compare.
+        col1 : str
+            The name of the first column to compare.
+        col2 : str
+            The name of the second column to compare.
+        col_match : str
+            The name of the output column that will store the comparison results.
+
+        Returns
+        -------
+        snowflake.snowpark.DataFrame
+            The DataFrame with an additional column containing the comparison results.
+
+        Raises
+        ------
+        Exception
+            If the comparison fails due to incompatible types or other issues,
+            returns a column of `False` values.
+        """
         try:
-            return pl.Series(
-                (col1.eq_missing(col2)) | (col1.is_null() & col2.is_null())
+            col1 = snowpark_normalize_string_column(
+                spf.col(col1), self.ignore_space, self.ignore_case
+            )
+            col2 = snowpark_normalize_string_column(
+                spf.col(col2), self.ignore_space, self.ignore_case
+            )
+
+            return dataframe.withColumn(
+                col_match,
+                spf.when(col1.eqNullSafe(col2), spf.lit(True)).otherwise(
+                    spf.lit(False)
+                ),
             )
         except Exception:
-            try:
-                return pl.Series(col1.cast(pl.String) == col2.cast(pl.String))
-            except Exception:
-                return pl.Series(False * col1.shape[0])
-
-    # pyspark implementation of compare
-    @compare.register
-    def _(self, col1: ps.Column, col2: ps.Column) -> ps.Column:
-        return (
-            psf.when(psf.col(col1).eqNullSafe(psf.col(col2)), psf.lit(True)).otherwise(
-                psf.lit(False)
-            ),
-        )
-
-    # snowpark implementation of compare
-    @compare.register
-    def _(self, col1: sp.Column, col2: sp.Column) -> sp.Column:
-        return (
-            spf.when(spf.col(col1).eqNullSafe(spf.col(col2)), spf.lit(True)).otherwise(
-                spf.lit(False)
-            ),
-        )
+            return dataframe.withColumn(col_match, spf.lit(False))
 
 
 def pandas_normalize_string_column(
