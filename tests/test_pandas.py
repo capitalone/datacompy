62d1601b176512dfb392b72db87c5d5ef69c688d
Samuel Shi <samuel.shi2004@gmail.com>
2026-02-10 21:09:37 -0500

    add unit test for missing sens. col

--- tests/test_pandas.py
+++ tests/test_pandas.py
@@ -2223,3 +2223,18 @@ def test_sensitive_columns_as_join_columns():
     assert len(compare.df1_unq_rows) == 1
     # Just render the report to make sure it renders.
     compare.report()
+
+
+def test_sensitive_columns_missing():
+    df1 = pd.DataFrame([{"a": 1, "b": "hello", "c": 3}, {"a": 1, "b": "67", "c": 6}])
+    df2 = pd.DataFrame([{"a": 1, "b": "hello", "d": 4}, {"a": 2, "b": "yo", "d": 7}])
+    compare = datacompy.PandasCompare(
+        df1, df2, join_columns=["a"], sensitive_columns=["b", "c"]
+    )
+    assert compare.df1.loc[0, "b"] != "hello"
+    assert compare.df1.loc[1, "b"] != "67"
+    assert compare.df1.loc[0, "c"] != 3
+    assert compare.df1.loc[1, "c"] != 6
+    assert len(compare.df1_unq_rows) == 1
+    # Just render the report to make sure it renders.
+    compare.report()
